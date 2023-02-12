744bd2db5b8e37e7f9bd41d5af60660bd3a324f2
Aleksandr <aleks@Aleksandrs-MacBook-Air.local>
2023-02-12 16:44:14 +0100

    test

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
           <div class="header__logo">
             <img width="38" src="./img/pizza-logo.svg" alt="Pizza logo" />
             <div>
-              <h1>React Pizza</h1>
+              <h1>React Pizza Polandn</h1>
               <p>самая вкусная пицца во вселенной</p>
             </div>
           </div>
