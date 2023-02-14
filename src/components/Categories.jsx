bf7ba8091ef8c4452fc3cdddd3cae074414a9fec
Aleks <05042016step@gmail.com>
2023-02-14 02:15:56 +0100

    Categories active class was made by click

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 
 function Categories(props) {
+  const [activeIndex, setActiveIndex] = React.useState(0)
+  const categories = ['All','Gold','Silver','Classic','Charity','Banknotes']
+
+  const setActiveClass =(index)=>{
+setActiveIndex(index)
+  }
     return (
         <div className="categories">
 <ul>
-  <li className="active">Все</li>
+  {categories.map((item,index)=>{
+    return <li key ={index} onClick={()=>{setActiveClass(index)}} className={(activeIndex===index)?"active":""}>{item}</li>
+  })}</ul>
+  {/* <ul><li className="active">Все</li>
   <li>Gold</li>
   <li>Silver</li>
   <li>Classic</li>
   <li>Charity</li>
   <li>Bons</li>
-</ul>
+</ul>  */}
 </div>
     );
 }
